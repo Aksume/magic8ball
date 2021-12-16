123f03fc396b43bfef37a0a15c0c988c024cf5e2
Aksume <84611765+Aksume@users.noreply.github.com>
2021-12-15 23:12:06 -0600

    update css

--- Final-project/js/scripts.js
+++ Final-project/js/scripts.js
@@ -1,4 +1,4 @@
-//generate the answers for the ball
+//generate the answers for the 8ball
 
 //array of answers
 var answers = [
@@ -8,44 +8,33 @@ var answers = [
 	"As I ee it, yes",
 	"Most likely",
 	"Outlook good",
-	"Yes",
 	"Don't count on it",
 	"My reply is no",
+	"Yes",
 	"My sources say no",
 	"Outlook not so good",
 	"Very doubtful",
 	"Ask again later",
 	"Cannot predict now",
 ];
-// when window.onload
-// The onload property processes load events after the element has finished loading
+
 window.onload = function () {
 	var eight = document.getElementById("eight");
 	var answer = document.getElementById("answer");
 	var Ask = document.getElementById("eight-ball");
 	var question = document.getElementById("question");
 	var h5 = document.querySelector("h5");
-	var divCircle = document.querySelector("#circle").style.animation =
-		"spin1 1s linear infinite";
-	// when the time interval  is high the animation is slow motion  and to the revers the lower the interval the animation keep continue
-
+	var divCircle = (document.querySelector("#circle").style.animation =
+		"spin1 1s linear infinite");
 	Ask.addEventListener("click", function () {
 		if (question.value.length < 1) {
 			alert("Enter a question!");
 		} else {
-			eight.innerText = ""; // 8 means a place holder of the answer
+			eight.innerText = ""; 			
 
-			// Math.random() returns a random number between 0 (inc) and 1 (exc):
-			// Math.floor() rounds a number DOWN to the nearest integer:
+			var num = Math.floor(Math.random() * answers.length); 
 
-			var num = Math.floor(Math.random() * answers.length); //to get a random answers message
-			// var num = Math.floor(0.6 * 14);
-			// var num = Math.floor(8.4); it gives me  a number down to the nerest int 
-			// var num = 8
-
-			answer.innerText = answers[num];
-
-			// question.remove();
+			answer.innerText = answers[num];		
 
 			question.hidden = true;
 
